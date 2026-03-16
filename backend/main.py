0275e404246f9e314805fd688d0c1faa95dec02d
aali006 <ashmat.ali0777@gmail.com>
2026-03-16 11:25:37 +0530

    commit04

--- backend/main.py
+++ backend/main.py
@@ -426,7 +426,7 @@ async def login_user(email: str = Form(...), password: str = Form(...)):
     return {
         "user_id": "U001",
         "name": "Admin User",
-        "role": "HR",
+        "role": "Interviewer",
         "email":email
     }
     
